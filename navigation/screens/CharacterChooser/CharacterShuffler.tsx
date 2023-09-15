911c8b9a34ec7c95d720cbfd3f2afc67e69e93a2
Arturo-Bravo <arturobravo1st@gmail.com>
2023-09-14 21:50:19 -0700

    characters are shuffled on render, character cards are rendered

--- navigation/screens/CharacterChooser/CharacterShuffler.tsx
+++ navigation/screens/CharacterChooser/CharacterShuffler.tsx
@@ -1,5 +1,7 @@
 import {Text, View, StyleSheet, Image, FlatList, Button} from 'react-native';
 import {RouteProp} from '@react-navigation/native';
+import {useCallback, useEffect, useState} from 'react';
+import {CHARACTERS} from '../../../utils/characters';
 
 export type RootStackParamList = {
   CharacterShuffler: {
@@ -13,16 +15,42 @@ interface IProps {
 }
 
 const CharacterShuffler = (props: IProps) => {
+  const [shuffledArray, setShuffledArray] = useState<number[] | null>(null);
   const {characters, numPlayers} = props.route.params;
-  console.log(props.route.params);
+
+  const shuffleArray = useCallback(() => {
+    const shuffledArray = [...characters]; // Create a copy of the original array
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    setShuffledArray(shuffledArray.slice(0, +numPlayers));
+  }, [characters]);
+
+  useEffect(() => {
+    shuffleArray();
+  }, [characters, numPlayers]);
   return (
     <View>
       <Text style={{color: 'black'}}>CharacterShuffler</Text>
-      <Text style={{color: 'black'}}>
-        characters: {characters} number of players: {numPlayers}{' '}
-      </Text>
+      <View>
+        {shuffledArray?.map((id, index) => (
+          <View key={id}>
+            <Text style={styles.text}>Player: {index + 1}</Text>
+            <Image source={CHARACTERS[id].img} />
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+  },
+});
 
 export default CharacterShuffler;
